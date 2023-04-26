d93d444ad7e6216aee979c17d60e84f2357083f9
Gaurav Chakraborty <gairavchakraborty472@gmail.com>
2023-04-26 11:30:49 +0530

    Keeper app level 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
-import React  from "react";
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-function App(){
+import notes from "../notes"
 
-    return <div>
 
-        <Header />
-        <Footer />
-        <Note />
-    </div>
+function App() {
+
+    return (
+        <div>
+
+            <Header />
+ 
+            {notes.map( (noteItems => 
+                 
+                 <Note
+                    key={noteItems.key}
+                    title={noteItems.title}
+                    content={noteItems.content}
+                />
+
+            ))}
+            <Footer />
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-function Note(){
+function Note(props){
 
     return(
 
         <div className="note">
-            <h1> This is a Title</h1>
-            <p> This is a content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
