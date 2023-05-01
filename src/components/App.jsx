1503861ff16ccf9fe35780ffe06b8be39d3c9ad7
Gaurav Chakraborty <gairavchakraborty472@gmail.com>
2023-05-01 19:32:52 +0530

    Keeper app Level 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes"
-
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-    return (
-        <div>
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-            <Header />
- 
-            {notes.map( (noteItems => 
-                 
-                 <Note
-                    key={noteItems.key}
-                    title={noteItems.title}
-                    content={noteItems.content}
-                />
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-            ))}
-            <Footer />
-
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
